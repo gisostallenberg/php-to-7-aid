393cb3a123f7061dc8ee0bb361fa449a1d90fe72
Giso Stallenberg <gisostallenberg@gmail.com>
2015-06-28 17:51:01 +0200

    Added abstract classes and interfaces for solvers and fixers

---
+++ src/AbstractSolver.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * This file is part of the PHP To 7 Aid project.
+ *
+ * (c) Giso Stallenberg <gisostallenberg@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace GisoStallenberg\phpTo7aid;
+
+abstract class AbstractSolver implements SolverInterface {
+    
+}
